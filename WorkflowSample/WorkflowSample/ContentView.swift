54d911de921e1b72ed498d2920df6a11add4a60d
Tariq <tariq2305@gmail.com>
2020-04-23 11:16:53 +0800

    - Initial commit

---
+++ WorkflowSample/WorkflowSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkflowSample
+//
+//  Created by Tariq Mohammad on 23/4/20.
+//  Copyright © 2020 Visa Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
